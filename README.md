1e75b8a90f7b7cb986d0f789b5a5fd5f9f874ce9
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-01-01 13:37:35 +0700

    First initial code

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # dovecot-log-scrapper
+
+### Requirements
+
+- node v0.11
+- npm
+
+### How to
+
+- ``npm install``
+- ``node index.js /path/to/file.log  databaseName``
+
+### Note
+
+- The log file should have date format like ``2015-12-28T17:13:41.290881+07:00``. Example :
+
+<pre>
+2015-12-28T17:13:51.289892+07:00 mailgw [dovecot]	 info	:  imap-login: Login: user=<username@somedomain.com>, method=PLAIN, rip=1.1.1.1, lip=1.1.1.2, mpid=5372, session=<y7zAj/In7wAKHwJO>
+</pre>
---
+++ index.js
@@ -0,0 +1,141 @@
+var _ = require("lodash");
+var fs = require("fs");
+var ReadLine = require("readline");
+var moment = require("moment");
+var mongoose = require("mongoose");
+var db = process.argv[3] || "test";
+var path = process.argv[2] || "/var/log/mail.info";
+var exec = require('child_process').exec;
+var prefix = "__webmail__";
+var clientTypes = ["webmail","imap","pop3"];
+
+if (mongoose.connection.readyState === 0) {
+  mongoose.connect("mongodb://localhost/" + db);
+}
+mongoose.connection.once("open", function(){
+  mongoose.connection.db.collection("users", start);
+});
+
+var prepare = function(cb) {
+  mongoose.connection.db.collection("domains", function(err, Domains){
+    if (err) {
+      console.log(err);
+      process.exit();
+    }
+    Domains.find()
+      .toArray(function(err, domains){
+        if (err) {
+          console.log(err);
+          process.exit();
+        }
+        exec("wc -l " + path, function (err, total) {
+          if (err) {
+            console.log(err);
+            process.exit();
+          }
+          total = parseInt(total.split(" ")[0]);
+          cb(domains, total);
+        });
+      })
+  })
+}
+
+var start = function(err, Users){
+  if (err) {
+    console.log(err);
+    process.exit();
+  }
+  prepare(function(domainMap, totalLines){
+    var count = 0;
+    var notFound = [];
+    var rd = ReadLine.createInterface({
+      input : fs.createReadStream(path),
+      output : process.stdout,
+      terminal : false,
+    });
+    rd.on("line", function(line){
+      /* console.log(line); */
+      count++;
+      if (line.length === 0) {
+        return;
+      }
+      var accessLog = {};
+      var email = "";
+      var splitted = line.split(" pnsmailgw [dovecot]");
+      accessLog.lastActivity = new Date(splitted[0]);
+      var log = splitted[1];
+    
+      if (log.indexOf("Login: user=") === -1) {
+        return;
+      }
+      log = log.split(",")[0];
+      email = log.split("<")[1].split(">")[0];
+      if (email.indexOf(prefix) > -1) {
+        email = email.replace(prefix, "");
+        accessLog.lastClientType = "webmail";
+      } else {
+        accessLog.lastClientType =  log.split("-login")[0]
+                                      .replace(/ /g,"")
+                                      .split(":")[1];
+      }
+      var domain = email.split("@")[1];
+      var username = email.split("@")[0];
+
+      _.some(domainMap, function(d){
+        if (d.name === domain) {
+          domain = d._id;
+          return
+        }
+      });
+      // VALUE CHECK
+
+      // Should have a valid clientType
+      var isValidClientType = _.some(clientTypes, function(clientType){
+        return clientType === accessLog.lastClientType;
+      });
+      if (!isValidClientType) {
+        console.log("Not a valid clientType");
+        console.log(line);
+        process.exit();
+      }
+      // Should have a valid date
+      if (accessLog.lastActivity.toString() === "Invalid Date") {
+        console.log("Not a valid date value");
+        console.log(line);
+        process.exit();
+      }
+
+      console.log(accessLog);
+      var query = {
+        username : username,
+        domain : domain
+      }
+      Users.findOne(query, function(err, result){
+        if (!result) {
+          if (notFound.indexOf(username) === -1) {
+            notFound.push(username);
+          }
+          return;
+        }
+        if (result.accessLog && 
+        result.accessLog.lastActivity &&
+        result.accessLog.lastActivity > accessLog.lastActivity) {
+          return;
+        }
+        Users.findOneAndUpdate(query,
+          { $set : { accessLog : accessLog }}, 
+          {upsert : true }, 
+        function(err, result){
+          if (err) {
+            console.log(err);
+          }
+          if (count === totalLines) {
+            console.log("Done!");
+            console.log(notFound);
+            process.exit();
+          }
+        })
+      })
+    })
+  })
+}
